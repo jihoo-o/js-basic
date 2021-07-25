2ec30db31291530e3530ea482a503350e004b1e6
seonhwa <seonhwakei@gmail.com>
2021-07-25 22:29:11 +0900

    Javascript: Responsive navbar + navbar btn click

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ function onDocumentScroll() {
   }
 }
 document.addEventListener('scroll', onDocumentScroll);
+
+const navbarMenu = document.querySelector('.navbar__menu');
+const contactBtn = document.querySelector('.home__contact');
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const skills = document.querySelector('#skills');
+const work = document.querySelector('#work');
+const testimonials = document.querySelector('#testimonials');
+const contact = document.querySelector('#contact');
+function onBtnClick(event) {
+  const link = event.target.dataset.link;
+  console.log(link);
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  if (link === '#home') {
+    scrollTo.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  } else {
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+navbarMenu.addEventListener('click', onBtnClick);
+contactBtn.addEventListener('click', onBtnClick);
