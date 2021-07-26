2a222e0b4e8dc266ddc8a47027a36ec56c233746
seonhwa <seonhwakei@gmail.com>
2021-07-26 09:22:43 +0900

    Javascript: Home contact btn

--- main.js
+++ main.js
@@ -12,26 +12,27 @@ function onDocumentScroll() {
 }
 document.addEventListener('scroll', onDocumentScroll);
 
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-const contactBtn = document.querySelector('.home__contact');
-const home = document.querySelector('#home');
-const about = document.querySelector('#about');
-const skills = document.querySelector('#skills');
-const work = document.querySelector('#work');
-const testimonials = document.querySelector('#testimonials');
-const contact = document.querySelector('#contact');
-function onBtnClick(event) {
+navbarMenu.addEventListener('click', (event) => {
   const link = event.target.dataset.link;
-  console.log(link);
   if (link == null) {
     return;
   }
-  const scrollTo = document.querySelector(link);
-  if (link === '#home') {
+  scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  if (selector === '#home') {
     scrollTo.scrollIntoView({ behavior: 'smooth', block: 'center' });
   } else {
     scrollTo.scrollIntoView({ behavior: 'smooth' });
   }
 }
-navbarMenu.addEventListener('click', onBtnClick);
-contactBtn.addEventListener('click', onBtnClick);
